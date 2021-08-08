0ff0cc04e228adf01e5564d10f2c5619253a6a07
oknwachukwu <okwy.m.nwachukwu@gmail.com>
2021-08-08 19:30:12 +0100

    Changed the MyApp class to extend StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,58 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+@override
+  State<StatefulWidget> createState() {
+    return MyAppPage();
+  }
+}
+
+class MyAppPage extends State<MyApp>{
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex++;
+    print('Answered question $questionIndex');
+  }
+
+  // void answerQuestion2() {
+  //   print('Pressed Button 2');
+  // }
+
+  // void answerQuestion3() {
+  //   print('Pressed Button 3');
+  // }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your name?',
+      'What\'s your favorite color?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Text('Hello'),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            // Text('Hello'),
+            ElevatedButton(
+              child: Text('Button 1'),
+              onPressed: () => answerQuestion(),
+            ),
+            ElevatedButton(
+              child: Text('Button 2'),
+              onPressed: () => answerQuestion(),
+            ),
+            ElevatedButton(
+              child: Text('Button 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
